97e942429e89155d57e137df83f81d52b00e1bd5
Deshna <deshnakeys@yahoo.com>
2022-05-31 15:56:35 -0500

    deploy updated

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@ deploy python apps x
 deploy in the dev envn
 deployment5 master
 deployment4
-<<<<<<< HEAD
+HEAD
 deployment to stagin
 echo good job.
 tested and acceptedg
-=======
  HEAD
 deployment to stagin
 echo good job.
 tested and acceptedg
 22dabe7 (tested and approved by the client)
->>>>>>> 115aefb (conflict 2 resolved)
+115aefb (conflict 2 resolved)
+shh key added
